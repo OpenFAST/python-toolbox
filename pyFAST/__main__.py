f3c19d3f1893f40ed7f4bddd709f6f29148d6395
Rob Hammond <RHammond2@users.noreply.github.com>
2019-12-04 09:27:13 -0700

    Update __main__.py (#9)
    * update pyfast to produce plots and case summary (#4)
    
    * update pyfast to run properly through command line

--- pyFAST/__main__.py
+++ pyFAST/__main__.py
@@ -25,6 +25,105 @@ def match_cases(case_regex: str) -> List[str]:
 
 
 def main():
+    """Runs the pyFAST suite.
+    """
+
+    parser = argparse.ArgumentParser(
+        description="Calculates the regression test norms.", prog="pyfast"
+    )
+
+    parser.add_argument(
+        "-r",
+        "--regex-case",
+        dest="case",
+        type=str,
+        nargs="+",
+        default=["all"],
+        required=False,
+        help=(
+            '"Regex" case names where the text. Looks to see if the provided'
+            "string is contained in any of the valid cases. "
+            "Note: not case sensitive"
+        ),
+    )
+    parser.add_argument(
+        "-e",
+        dest="executable",
+        type=str,
+        nargs="+",
+        required=True,
+        help="Test-produced data file.",
+    )
+
+    parser.add_argument(
+        "-s",
+        dest="source",
+        type=str,
+        required=True,
+        help="Path to the OpenFAST repository",
+    )
+    parser.add_argument(
+        "-c",
+        dest="compiler",
+        type=str,
+        required=True,
+        choices=["intel", "gnu"],
+        help="Compiler ID for the system",
+    )
+
+    parser.add_argument(
+        "-t",
+        "--tol",
+        dest="tolerance",
+        type=float,
+        default=1e-5,
+        help="Tolerance for determing the failure of a norm",
+    )
+    parser.add_argument(
+        "--plot",
+        dest="plot",
+        choices=[0, 1, 2],
+        type=int,
+        default=0,
+        help="0: no plots; 1: all plots; 2: plot failure cases only",
+    )
+    parser.add_argument(
+        "-j",
+        "--jobs",
+        dest="jobs",
+        type=int,
+        default=-1,
+        help="Number of cases to run in parallel. Use -1 for 80 pct of available cores",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        help="Use flag to use verbose output.",
+    )
+    parser.add_argument(
+        "-n",
+        "--no-execution",
+        dest="no_execution",
+        action="store_true",
+        help="Doesn't run the actual test case(s). The data should already exist.",
+    )
+    # Doesn't run openfast, uses pre-compiled results
+
+    parser.add_argument(
+        "--norm",
+        dest="norm_list",
+        type=str,
+        nargs="+",
+        choices=["max_norm", "max_norm_over_range", "l2_norm", "relative_l2_norm"],
+        help="The norm(s) to be computed.",
+    )
+
+    # Parse the arguments, find the cases to be run, and initialize the openFAST
+    # execution class
+    args = parser.parse_args()
+
     cases = (
         "all"
         if "all" in args.case
@@ -68,96 +167,6 @@ def main():
     reg_test.create_results_summary(cases, attributes, norm_res, norm_list, plots)
 
 
-parser = argparse.ArgumentParser(
-    description="Calculates the regression test norms.", prog="pyfast"
-)
+if __name__ == "__main__":
 
-parser.add_argument(
-    "-r",
-    "--regex-case",
-    dest="case",
-    type=str,
-    nargs="+",
-    default=["all"],
-    required=False,
-    help=(
-        '"Regex" case names where the text. Looks to see if the provided'
-        "string is contained in any of the valid cases. "
-        "Note: not case sensitive"
-    ),
-)
-parser.add_argument(
-    "-e",
-    dest="executable",
-    type=str,
-    nargs="+",
-    required=True,
-    help="Test-produced data file.",
-)
-
-parser.add_argument(
-    "-s", dest="source", type=str, required=True, help="Path to the OpenFAST repository"
-)
-parser.add_argument(
-    "-c",
-    dest="compiler",
-    type=str,
-    required=True,
-    choices=["intel", "gnu"],
-    help="Compiler ID for the system",
-)
-
-parser.add_argument(
-    "-t",
-    "--tol",
-    dest="tolerance",
-    type=float,
-    default=1e-5,
-    help="Tolerance for determing the failure of a norm",
-)
-parser.add_argument(
-    "--plot",
-    dest="plot",
-    choices=[0, 1, 2],
-    type=int,
-    default=0,
-    help="0: no plots; 1: all plots; 2: plot failure cases only",
-)
-parser.add_argument(
-    "-j",
-    "--jobs",
-    dest="jobs",
-    type=int,
-    default=-1,
-    help="Number of cases to run in parallel. Use -1 for 80 pct of available cores",
-)
-parser.add_argument(
-    "-v",
-    "--verbose",
-    dest="verbose",
-    action="store_true",
-    help="Use flag to use verbose output.",
-)
-parser.add_argument(
-    "-n",
-    "--no-execution",
-    dest="no_execution",
-    action="store_true",
-    help="Doesn't run the actual test case(s). The data should already exist.",
-)
-# Doesn't run openfast, uses pre-compiled results
-
-parser.add_argument(
-    "--norm",
-    dest="norm_list",
-    type=str,
-    nargs="+",
-    choices=["max_norm", "max_norm_over_range", "l2_norm", "relative_l2_norm"],
-    help="The norm(s) to be computed.",
-)
-
-# Parse the arguments, find the cases to be run, and initialize the openFAST
-# execution class
-args = parser.parse_args()
-
-main()
+    main()
