4c28e90a8d427ebed5673f0744b61c74c6db7b18
Rafael M Mudafort <rafmudaf@gmail.com>
2020-07-30 16:05:31 -0500

    Remove seaborn import

--- pyFAST/aeroacoustics/plot_directivity.py
+++ pyFAST/aeroacoustics/plot_directivity.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import weio
 from parse import *
 import re
-import seaborn as sb
 import os
 
 #########################################################################################################################################
