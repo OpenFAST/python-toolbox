0945d72d14f2f89ff9cb9a14da58ba63b36ecfbc
dzalkind <dzalkind@nrel.gov>
2021-01-15 17:46:28 -0700

    Streamline runLinTurbine example

--- pyFAST/linearization/example/runLinTurbine.py
+++ pyFAST/linearization/example/runLinTurbine.py
@@ -2,177 +2,111 @@ import numpy as np
 import pandas as pd
 import os
 import pyFAST.linearization.linearization as lin
-import pyFAST.linearization.LinearModel as lin_mod
+# import pyFAST.linearization.LinearModel as lin_mod
 import pyFAST.case_generation.case_gen as case_gen
 import pyFAST.input_output.fast_output_file as fo
 import pyFAST.input_output.rosco_input_file as ri
 import sys
-from ROSCO_toolbox import utilities as ROSCO_utilities
-from ROSCO_toolbox import controller as ROSCO_controller
-from ROSCO_toolbox import turbine as ROSCO_turbine
+# from ROSCO_toolbox import utilities as ROSCO_utilities
+# from ROSCO_toolbox import controller as ROSCO_controller
+# from ROSCO_toolbox import turbine as ROSCO_turbine
 import pyFAST.case_generation.runner as runner
-from pCrunch.Analysis import Loads_Analysis
+# from pCrunch.Analysis import Loads_Analysis
 import yaml
+import scipy as sp
 
 
 
 import matplotlib.pyplot as plt
 
+def run_linearization():
+    """ Example to run a set of OpenFAST simulations (linearizations)
+
+    This script uses a reference directory (`ref_dir`) which contains a reference input file (.fst)
+    1) The reference directory is copied to a working directory (`out_dir`).
+    2) All the fast input files are generated in this directory based on a list of dictionaries (`PARAMS`).
+    For each dictionary in this list:
+       - The keys are "path" to a input parameter, e.g. `EDFile|RotSpeed`  or `FAST|TMax`.
+         These should correspond to the variables used in the FAST inputs files.
+       - The values are the values corresponding to this parameter
+    For instance:
+         PARAMS[0]['DT']                    = 0.01
+         PARAMS[0]['EDFile|RotSpeed']       = 5
+         PARAMS[0]['InflowFile|HWindSpeed'] = 10
+
+    3) The simulations are run, successively distributed on `nCores` CPUs.
+    4) The output files are read, and averaged based on a method (e.g. average over a set of periods,
+        see averagePostPro in postpro for the different averaging methods).
+       A pandas DataFrame is returned
+
+    """
+    # --- Parameters for this script
+    of_dir           = '/Users/dzalkind/Tools/openfast-dev'  # openfast dir, using dev branch as of Jan-14
+    this_dir         = os.path.dirname(__file__)
+    ref_dir          = '/Users/dzalkind/Tools/ROSCO_toolbox/Test_Cases/NREL-5MW'   # Folder where the fast input files are located (will be copied)
+    out_dir          = os.path.join(this_dir,'NREL-5MW_Linear/')     # Output folder (will be created)
+    main_file        = 'NREL-5MW.fst'  # Main file in ref_dir, used as a template
+    FAST_EXE         = os.path.join(of_dir,'install/bin/openfast') # Location of a FAST exe (and dll)
+
+    # --- Defining the parametric study  (list of dictionnaries with keys as FAST parameters)
+    WS = [14,16,18]
+    BaseDict = {'TMax': 600}
+    BaseDict = case_gen.paramsLinearTrim(BaseDict)   # Run linear trim case
+
+    PARAMS=[]
+    for i,wsp in enumerate(WS): 
+        p=BaseDict.copy()
+        p['InflowFile|HWindSpeed']  = wsp
+        p['InflowFile|WindType']    = 1 # Setting steady wind
+        
+        # Set DOFs
+        p['EDFile|GenDOF']          = 'True'
+        p['EDFile|FlapDOF1']         = 'True'
+        p['EDFile|TwFADOF1']         = 'True'
+
+        # Set number of linearizations
+        p['NLinTimes']              = 12
+
+        p['__name__']='{:03d}_ws{:04.1f}'.format(i,p['InflowFile|HWindSpeed'])
+        PARAMS.append(p)
+        i=i+1
+    # --- Generating all files in a output directory
+    fastfiles=case_gen.templateReplace(PARAMS,ref_dir,outputDir=out_dir,removeRefSubFiles=True,main_file=main_file, oneSimPerDir=False)
+    print(fastfiles)
+
+    # --- Creating a batch script just in case
+    runner.writeBatch(os.path.join(out_dir,'_RUN_ALL.bat'),fastfiles,fastExe=FAST_EXE)
+    # --- Running the simulations
+    runner.run_fastfiles(fastfiles,fastExe=FAST_EXE,parallel=True,showOutputs=True,nCores=4)
+
+    # --- Simple Postprocessing
+    # (averaging each signal over the last period for each simulation)
+    outFiles = [os.path.splitext(f)[0]+'.outb' for f in fastfiles]
+    # avg_results = postpro.averagePostPro(outFiles, avgMethod='periods', avgParam=1, ColMap = {'WS_[m/s]':'Wind1VelX_[m/s]'},ColSort='WS_[m/s]')
+    # avg_results.drop('Time_[s]',axis=1, inplace=True)
+
+    return outFiles
+
 
 
 if __name__ == '__main__':
 
-    # Load matlab systems, before using Emmanuel's tools
-    if True:
-        linTurb = lin_mod.LinearTurbineModel('/Users/dzalkind/Tools/matlab-toolbox/Simulations/SaveData/LinearModels/PitTwr.mat', \
-            fromMat=True)
-    else:
-        lin_file_dir = '/Users/dzalkind/Tools/SaveData/TrimTest/LinearTwrPit_Tol1en5'
-        linTurb = lin_mod.LinearTurbineModel(lin_file_dir,reduceStates=False)
+    # 1. Run linearizations
+    # outfiles = run_linearization()
 
+    outfiles = [
+    '/Users/dzalkind/Tools/python-toolbox/pyFAST/linearization/example/NREL-5MW_Linear/000_ws14.0.out',
+    '/Users/dzalkind/Tools/python-toolbox/pyFAST/linearization/example/NREL-5MW_Linear/001_ws16.0.out',
+    '/Users/dzalkind/Tools/python-toolbox/pyFAST/linearization/example/NREL-5MW_Linear/002_ws18.0.out'
+    ]
 
-    # load disturbance from file
-    fast_out = fo.FASTOutputFile('/Users/dzalkind/Tools/matlab-toolbox/Simulations/SaveData/072720_183300.out')
-    fast_out._read()
+    # 2. Do MBC
+    MBC = lin.run_pyMBC(outfiles)
 
-    wind_ind = fast_out.info['attribute_names'].index('RtVAvgxh')
-    u_h      = fast_out.data[:, wind_ind]
-
-    tt       = fast_out.data[:, fast_out.info['attribute_names'].index('Time')]
-
-    
-
-    # linTurb.solve(tt,u_h)
-
-    # load controller file
-    # f = ri.ROSCOInputFile('/Users/dzalkind/Tools/SaveData/TrimTest/LinearTwrPit_Tol1en5/lin_19_DISCON.IN')
-
-    # a = f['LoggingLevel']
-
-    if False:
-        fp = ROSCO_utilities.FileProcessing()
-        f = fp.read_DISCON('/Users/dzalkind/Tools/SaveData/Float_Test/UM_DLC0_100_DISCON.IN')
-
-        linCont = lin_mod.LinearControlModel([],fromDISCON_IN=True,DISCON_file=f)
-    else:
-        # Load yaml file 
-        parameter_filename = '/Users/dzalkind/Tools/ROSCO_toolbox/Tune_Cases/IEA15MW.yaml'
-        inps = yaml.safe_load(open(parameter_filename))
-        path_params         = inps['path_params']
-        turbine_params      = inps['turbine_params']
-        controller_params   = inps['controller_params']
-
-        # Instantiate turbine, controller, and file processing classes
-        turbine         = ROSCO_turbine.Turbine(turbine_params)
-        controller      = ROSCO_controller.Controller(controller_params)
-
-        # Load turbine data from OpenFAST and rotor performance text file
-        turbine.load_from_fast(path_params['FAST_InputFile'],path_params['FAST_directory'],dev_branch=True,rot_source='txt',txt_filename=path_params['rotor_performance_filename'])
-
-        # Tune controller 
-        controller.tune_controller(turbine)
-
-        controller.turbine = turbine
-
-        linCont = lin_mod.LinearControlModel(controller)
-
-
-    Lin_OutList, Lin_OutData, P_cl = linTurb.solve(tt,u_h,Plot=False,open_loop=False,controller=linCont)
-
-    Non_OutList = fast_out.info['attribute_names']
-    Non_OutData = fast_out.data
-
-
-
-    # comparison plot
-    if False:
-        comp_channels = ['RtVAvgxh','GenSpeed','TwrBsMyt','PtfmPitch']
-        ax = [None] * len(comp_channels)
-        plt.figure(2)
-
-        for iPlot in range(0,len(comp_channels)):
-            ax[iPlot] = plt.subplot(len(comp_channels),1,iPlot+1)
-            try:
-                ax[iPlot].plot(tt,Non_OutData[:,Non_OutList.index(comp_channels[iPlot])])
-            except:
-                print(comp_channels[iPlot] + ' is not in OpenFAST OutList')
-
-            try:
-                ax[iPlot].plot(tt,Lin_OutData[:,Lin_OutList.index(comp_channels[iPlot])])
-            except:
-                print(comp_channels[iPlot] + ' is not in Linearization OutList')
-            ax[iPlot].set_ylabel(comp_channels[iPlot])
-            ax[iPlot].grid(True)
-            if not iPlot == (len(comp_channels) - 1):
-                ax[iPlot].set_xticklabels([])
-
-        plt.show()
-
-
-    # sweep controller.pc_omega and run linearization
-    fast_data_lin = []
-
-    if True:
-        ww = np.linspace(.05,.45,6)
-
-        for om in ww:
-            # Tune controller 
-            controller.omega_pc = om
-            controller.tune_controller(turbine)
-
-            controller.turbine = turbine
-
-            # update linear controller
-            linCont = lin_mod.LinearControlModel(controller)
-
-            # solve 
-            Lin_OutList, Lin_OutData, P_cl = linTurb.solve(tt,u_h,Plot=False,open_loop=False,controller=linCont)
-
-            # convert into pCrunch form
-            fd_lin  = {}
-            fd_lin['TwrBsMyt'] = Lin_OutData[:,Lin_OutList.index('TwrBsMyt')]
-            fd_lin['meta']    = {}
-            fd_lin['meta']['name'] = 'omega: ' + str(om)
-            fast_data_lin.append(fd_lin)
-
-
-
-            comp_channels = ['RtVAvgxh','GenSpeed','TwrBsMyt','PtfmPitch']
-            ax = [None] * len(comp_channels)
-            plt.figure(3)
-
-            for iPlot in range(0,len(comp_channels)):
-                ax[iPlot] = plt.subplot(len(comp_channels),1,iPlot+1)
-
-                try:
-                    ax[iPlot].plot(tt,Lin_OutData[:,Lin_OutList.index(comp_channels[iPlot])])
-                except:
-                    print(comp_channels[iPlot] + ' is not in Linearization OutList')
-                ax[iPlot].set_ylabel(comp_channels[iPlot])
-                ax[iPlot].grid(True)
-                if not iPlot == (len(comp_channels) - 1):
-                    ax[iPlot].set_xticklabels([])
-
-
-            
-
-        plt.show()
-        
-
-    # Try some post processing using pCrunch
-    chan_info = ('TwrBsMyt',4)
-
-    la = Loads_Analysis()
-    fDEL = la.get_DEL(fast_data_lin,chan_info)
-
-    fd_non  = {}
-    fd_non['TwrBsMyt'] = Non_OutData[:,Non_OutList.index('TwrBsMyt')]
-    fd_non['meta']    = {}
-    fd_non['meta']['name'] = 'nonlinear'
-
-    fDEL_nl = la.get_DEL([fd_non],chan_info)
-    print('here')
-
-
-
+    # Check natural frequencies against matlab
+    for mbc in MBC:
+        eigs = sp.linalg.eig(mbc['AvgA'])[0]
+        nat_freq_hz = (np.abs(eigs)/2/np.pi)
+        nat_freq_hz.sort()
+        print('Natural Freqs. (hz): {}'.format(nat_freq_hz))
 
